c727b498b4a6720615d7c8d9d66dfc4dcc6257bd
nshaibu <nafiushaibu1@gmail.com>
2017-03-29 00:53:13 +0000

    resolving bugs

--- QBox/bash_s/direct_linux_boot.sh
+++ QBox/bash_s/direct_linux_boot.sh
@@ -21,29 +21,33 @@
 
 : ${LIB_DIR:=$HOME/my_script/QB}
 
-. ${LIB_DIR}/include
+. ${LIB_DIR}/include '<random_uid.h>'
+
+DEFINE DIRECT_LINUX_BOOT
+
 
 if NOT_DEFINE ${CURSES_DIALOG_H} || NOT_DEFINE ${INIT_SH} || NOT_DEFINE ${BASIC_UTILS_H}; then
-	. ${LIB_DIR}/include '%%IMPORT%%' 'init.sh'
+	#${LIB_DIR}/include "%%IMPORT%%" 'init.sh'
 	. ${LIB_DIR}/include '<curses_dialog.h>'
 	. ${LIB_DIR}/include '<basic_utils.h>'
 fi 
 
-if NOT_DEFINE ${ERROR_H}; then
+if NOT_DEFINE ${ERROR_H} || NOT_DEFINE ${ARCHITECTURE_H} || NOT_DEFINE ${BOOT_SYSTEM_H}; then
 	. ${LIB_DIR}/include '<error.h>'
+	. ${LIB_DIR}/include '<architecture.h>'
+	. ${LIB_DIR}/include '<boot_system.h>'
 fi 
 
 
-  
-let test=1
-echo ${GO}
-exit 0
+Disk_Name=${RANDOM_UID}.img
+let "_save_=${FAILURE}"
+
 #while [ $test -eq 1 ]; do 
 	exec 3>&1
 		value=$(${DIALOG} \
-				--no-shadow --trim --ok-label "Boot" --output-separator "|" --form "Linux Direct Boot" ${HEIGHT} ${WIDTH} 13 \
-				"Machine Name:" 2 2 "" 3 2 $((WIDTH-8)) 50 "Kernel Image:" 5 2 "" 6 2 $((WIDTH-8)) 50 "Initial Ram Disk:" 8 2 "" 9 2 $((WIDTH-8)) 50 \
-				"Kernel Commad Line:" 11 2 "" 12 2 $((WIDTH-8)) 50 2>&1 1>&3)
+				--no-shadow --trim --ok-label "Boot" --output-separator "|" --form "Linux Direct Boot" ${HEIGHT} ${WIDTH} 11 \
+				"Machine Name:" 1 2 "" 2 2 $((WIDTH-8)) 50 "Kernel Image:" 4 2 "" 5 2 $((WIDTH-8)) 50 "Initial Ram Disk:" 7 2 "" 8 2 $((WIDTH-8)) 50 \
+				"Kernel Commad Line:" 10 2 "" 11 2 $((WIDTH-8)) 50 2>&1 1>&3)
 			
 			let "test_return=$?"
 	exec 3>&-
@@ -55,43 +59,93 @@ exit 0
 			VM_NAME=$(String_to_Upper ${value%%|*})
 			KERNEL=$(echo ${value} | cut -d '|' -f2)
 			INITRD=$(echo ${value} | cut -d '|' -f3)
-			KERNEL_CMD=$(echo $value | cut -d "|" -f4)
+			KERNEL_CMD=$(echo ${value} | cut -d "|" -f4)
 			
-#			KERNEL=`echo $2 | gawk -F "|" '{print $2}'` && KERNEL="-kernel ${KERNEL}"
-#			INITRD=`echo $2 | gawk -F "|" '{print $3}'` && INITRD="-initrd ${INITRD}"
-#			KERNEL_CMD=`echo $2 | gawk -F "|" '{print $4}'` && KERNEL_CMD="-append ${KERNEL_CMD}"
-		
-#			[ "${VM_NAME}" = "" ] && VM_NAME=MY_VM$$ || [ "`unique_vm_name $VM_NAME`" != "" ] && VM_NAME=MY_VM$$
-			let "TEST_ERROR_OCURRED=${SUCCESS}"
+			KERNEL_CMD="-append ${KERNEL_CMD}"
 			
+			#----------------Verify unique name-----------------------------
+			error_func_display $(err_str "VM_NAME:${STRERROR[vm_name]}:is_VMName_unique")
+			let "TEST_ERROR_OCURRED=$?"
+				
 			while [[ ${TEST_ERROR_OCURRED} -eq ${SUCCESS} ]]; do 
 				
-				error_func_display $(err_str "VM_NAME:${STRERROR[vm_name]}:is_VMName_unique")
-				TEST_ERROR_OCURRED=$?
-				
-				[[ ${TEST_ERROR_OCURRED} -eq ${SUCCESS} ]] && {
-					exec 3>&1
-						value=$(${DIALOG} \
-							--trim --colors --form "\Zb\Z0Name Not Unique\Zn\ZB\nThe name of the VM should be unique.The name should not \
-							contain \Zb\Z0white spaces\Zn\ZB and \"\Zb\Z0|\Zn\ZB\"." $((HEIGHT-5)) $((WIDTH-17)) 3 \
-							"Name:" 2 2 "" 2 7 $((WIDTH-26)) 18 2>&1 1>&3)
+				exec 3>&1
+					value=$(${DIALOG} \
+						--trim --colors --nocancel --form "\Zb\Z1Name not unique\Zn\ZB\nThe name of the VM should be unique.The name should not \
+						contain \Zb\Z0white spaces\Zn\ZB and \"\Zb\Z0|\Zn\ZB\"." $((HEIGHT-3)) $((WIDTH-12)) 3 \
+						"Name:" 2 2 "" 2 7 $((WIDTH-26)) 18 2>&1 1>&3)
+						
+				exec 3>&-
 					
-						let "test_return=$?"
-					exec 3>&-
-				}
+				VM_NAME=$(String_to_Upper ${value}) 
+				
+				error_func_display $(err_str "VM_NAME:${STRERROR[vm_name]}:is_VMName_unique")
+				let "TEST_ERROR_OCURRED=$?"	
 				
 			done
 			
-#			HD_IMG=${VM_NAME}_linuz.img
-#			qvm_name=${VM_NAME}_linuz.qvm
-#			VM_NAME=`echo ${VM_NAME} | gawk '{print toupper($0)}'` && VM_NAME="-name $VM_NAME"
-#		
-#			test_cmd=`echo ${KERNEL_CMD} | gawk '{print $2}'` && [ "$test_cmd" = "" ] && KERNEL_CMD=""
-#		
-#			bash ${QBOX_DIR}/bash_s/qemu-bootfile-generator.sh ${HD_IMG} ${QEMU} %${VM_NAME} % % % % % % % % \
-#			%${KERNEL} %${INITRD} % % % % %${KERNEL_CMD} % %
-#		
-#			bash ${QBOX_DIR}/bash_s/qemu-bootfile-reader.sh $qvm_name linuz
+			VM_NAME="-name ${VM_NAME}"
+			
+			#----------------------------kernel image ---------------------
+			error_func_display $(err_str "KERNEL:${STRERROR[KERNEL]}:check_is_file")
+			let "TEST_ERROR_OCURRED=$?"
+			
+			while [[ ${TEST_ERROR_OCURRED} -eq ${SUCCESS} ]]; do 
+				exec 3>&1
+					value=`${DIALOG} \
+						--no-shadow --colors --nocancel --clear --title "\Zb\Z1Select a kernel image\Zn\ZB" \
+						--fselect $HOME/ 10 50 2>&1 1>&3`
+				exec 3>&-
+				
+				KERNEL=${value}
+				
+				error_func_display $(err_str "KERNEL:${STRERROR[KERNEL]}:check_is_file")
+				let "TEST_ERROR_OCURRED=$?"
+			done 
+			
+			KERNEL="-kernel ${KERNEL}"
+			
+			#----------------------initial ram disk--------------------------
+			error_func_display $(err_str "INITRD:${STRERROR[INITRD]}:check_is_iso_file") $(err_str "INITRD:${STRERROR[INITRD]}:check_is_file")
+			let "TEST_ERROR_OCURRED=$?"
+			
+			while [[ ${TEST_ERROR_OCURRED} -eq ${SUCCESS} ]]; do 
+				exec 3>&1
+					value=`${DIALOG} \
+						--no-shadow --colors --nocancel --clear --title "\Zb\Z1Select a kernel image\Zn\ZB" \
+						--fselect $HOME/ 10 50 2>&1 1>&3`
+				exec 3>&-	
+				
+				INITRD=${value}
+				
+				error_func_display $(err_str "INITRD:${STRERROR[INITRD]}:check_is_iso_file") $(err_str "INITRD:${STRERROR[INITRD]}:check_is_file")
+				let "TEST_ERROR_OCURRED=$?"	
+			done 
+			
+			INITRD="-initrd ${INITRD}"
+			
+			#---------------------append---------------------------------
+			KERNEL_CMD="-append ${KERNEL_CMD}"
+			
+			#--------------save--------------------------------
+			exec 3>&1
+				value=`${DIALOG} \
+					--no-shadow --defaultno --colors --title "\Zb\Z0Direct Linux Boot\Zn\ZB" \
+					--yesno "\Zb\Z0Save VM\Zn\ZB\nDo you want to save this Virtual Machine ? Saving the VM will add an entry to the VM's database. This will allow you to boot the kernel without going through this steps again. \n\nThe default is \Zb\Z0NO\Zn\ZB" ${HEIGHT} ${WIDTH} 2>&1 1>&3`
+			exec 3>&-	
+				
+			case ${value} in 
+				${DIALOG_OK}) _save_=${SUCCESS} ;;
+				${DIALOG_CANCEL}) _save_=${FAILURE} ;;
+			esac
+			
+			detect_architecture "GLOBBING"
+			architecture_type_choice $?
+			
+			boot_system ${_save_}
+
 		;;
+		${DIALOG_CANCEL}) ;;
+		${DIALOG_BACK}) ;;
 	esac
 #done
