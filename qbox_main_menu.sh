df9679f620107517d59c717d7256db63bc97ec60
nshaibu <nafiushaibu1@gmail.com>
2017-04-03 01:08:13 +0000

    added event handler to clear pid qdb if vms are kill

--- qbox_main_menu.sh
+++ qbox_main_menu.sh
@@ -29,6 +29,9 @@ if NOT_DEFINE ${CURSES_DIALOG_H}; then
 	. ${LIB_DIR}/include '<curses_dialog.h>'
 fi 
 
+VM_clear_pid_qdb_eventhandler &
+clear_pids=$!
+
 while : ; do 
 	exec 3>&1
 		value=$(${DIALOG} \
@@ -48,7 +51,11 @@ while : ; do
 				
 				declare -a QDB_ARR=( $(init_database_qdb ${VMS_DB}) ) ##initialize qdb
 					
-				gen_str_=$(names_str_qdb ${QDB_ARR[@]}) #generate string to form qdb_arr 
+				if [[ ${#QDB_ARR[@]} -ne 0 ]]; then
+					gen_str_=$(names_str_qdb ${QDB_ARR[@]}) #generate string to form qdb_arr 
+				else 
+					gen_str_="1 No_Virtual_Machine_created_yet..."
+				fi 
 				
 				while true; do 
 					exec 3>&1
@@ -62,13 +69,15 @@ while : ; do
 					
 					case ${test_return} in 
 						${DIALOG_OK}) 
-							bootfile=${QDB_ARR[$(( value-1 ))]}
-							bootfile=${bootfile//\"/}
+							if [[ ${#QDB_ARR[@]} -ne 0 ]]; then
+								bootfile=${QDB_ARR[$(( value-1 ))]}
+								bootfile=${bootfile//\"/}
 							
-							boot_vm $(return_second_field ${bootfile}) $(return_first_field ${bootfile})
-							if [[ $? -eq ${SUCCESS} ]]; then
-								: #notify-send fullscn, keys shortcuts
-							fi 
+								boot_vm $(return_second_field ${bootfile}) $(return_first_field ${bootfile})
+								if [[ $? -eq ${SUCCESS} ]]; then
+									: #notify-send fullscn, keys shortcuts
+								fi
+							fi  
 						;;
 						${DIALOG_CANCEL}) break ;;
 					esac
@@ -76,9 +85,77 @@ while : ; do
 			elif [[ ${value} -eq 2 ]]; then
 				. ${LIB_DIR}/qbox_new_vm_menu.sh 
 			elif [[ ${value} -eq 3 ]]; then
-				:
+				gen_str_=""
+				declare -a QDB_ARR=( $(init_database_qdb ${PID_DB}) ) ##initialize qdb
+				
+				if [[ ${#QDB_ARR[@]} -ne 0 ]]; then
+					gen_str_=$(names_str_qdb ${QDB_ARR[@]})
+				else 
+					gen_str_="1 No_Virtual_Machine_is_running..."
+				fi 
+				
+				while true; do 
+					exec 3>&1
+						value=$(${DIALOG} \
+								--no-shadow --clear --cancel-label "Back" --colors --title "\Zb\Z0QBox VM Manager\Zn\ZB" \
+								--menu "\Zb\Z0QBox Menu\Zn\ZB\nManage Virtual machine." ${HEIGHT} ${WIDTH} \
+								7 ${gen_str_} 2>&1 1>&3)
+							
+						let "test_return=$?"
+					exec 3>&-				
+					
+					case ${test_return} in 
+						${DIALOG_OK}) 
+							[ ${#QDB_ARR[@]} -ne 0 ] && {
+								pid=$(return_second_field ${QDB_ARR[$(( value-1 ))]})
+								pid=${pid//\"/}
+								kill -9 ${pid} 2>/dev/null
+							} 
+						;;
+						${DIALOG_CANCEL}) break ;;
+					esac
+				done
 			elif [[ ${value} -eq 4 ]]; then
-				:
+				declare -a QDB_ARR=( $(init_database_qdb ${VMS_DB}) ) ##initialize qdb
+				
+				if [[ ${#QDB_ARR[*]} -ne 0 ]]; then
+					gen_str_=$(names_str_qdb ${QDB_ARR[@]})
+				else
+					gen_str_="1 No_Virtual_Machine_created_yet..."
+				fi 
+				
+				while true; do 
+					exec 3>&1
+						value=$(${DIALOG} \
+								--no-shadow --clear --cancel-label "Back" --colors --title "\Zb\Z0QBox VM Manager\Zn\ZB" \
+								--menu "\Zb\Z0QBox Menu\Zn\ZB\nManage Virtual machine." ${HEIGHT} ${WIDTH} \
+								8 ${gen_str_} 2>&1 1>&3)
+							
+						let "test_return=$?"
+					exec 3>&-				
+					
+					case ${test_return} in 
+						${DIALOG_OK}) 
+							if [[ ${#QDB_ARR[*]} -ne 0 ]]; then
+								declare -i qdb_index=${#QDB_ARR[@]}
+								declare -a QDB_COPY=( ${QDB_ARR[@]} )
+								
+								QDB_COPY[$(( qdb_index++ ))]="${value}"
+								QDB_COPY[$(( qdb_index++ ))]=${QDB_ARR[$(( value-1 ))]}
+								QDB_COPY[$(( qdb_index ))]=${VMS_DB}
+								
+								if delete_msg_qdb $(return_first_field ${QDB_ARR[$(( value-1 ))]}); then
+									declare -a QDB_ARR=( $(delete_val_qdb ${QDB_COPY[@]}) )
+									break
+								else
+									break
+								fi 
+							fi 
+						;;
+						${DIALOG_CANCEL}) break ;;
+					esac
+				done				
+				
 			elif [[ ${value} -eq 5 ]]; then
 				:
 			elif [[ ${value} -eq 6 ]]; then
@@ -89,7 +166,10 @@ while : ; do
 				:
 			fi
 		;;
-		${DIALOG_CANCEL}) break ;;
+		${DIALOG_CANCEL}) 
+			kill -9 ${clear_pids}
+			break 
+		;;
 	esac
 done 
 
